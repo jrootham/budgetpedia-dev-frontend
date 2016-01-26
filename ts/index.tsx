96886bb88f9d031b09e77ef6c141f6344691e639
Henrik Bechmann <henrik@therefore.ca>
2016-01-26 12:05:07 -0500

    change tabs to spaces

--- ts/index.tsx
+++ ts/index.tsx
@@ -4,6 +4,6 @@ import { render } from 'react-dom'
 import { Main } from './containers/main'
 
 render(
-	<Main />,
-	document.getElementById('main')
+    <Main />,
+    document.getElementById('main')
 )
