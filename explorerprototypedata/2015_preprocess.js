f786f4cd8dda9b6fec2768e4879cc3fab55a9c31
Henrik Bechmann <henrik@therefore.ca>
2016-04-24 22:00:15 -0400

    import 2015 staffing, expenses, revenues into prototype json struct 2015budger.json

--- explorerprototypedata/2015_preprocess.js
+++ explorerprototypedata/2015_preprocess.js
@@ -1,14 +1,126 @@
 // 2015_preprocess.js
 'use strict'
 var jsonfile = require('jsonfile')
-var csv = require('csv')
-var parse = csv.parse
-
+var fs = require('fs')
+// var csv = require('csv')
+var parse = require('csv-parse/lib/sync')
 var budgetroot = require('./2015_data/budgetroot.json')
-var expensedata = require('./2015_data/2015.expenses.expenditures.csv')
-var revenuedata = require('./2015_data/2015.revenues.expenditures.csv')
-var staffingdata = require('./2015_data/2015.staffing.programs.csv')
 
-parse(staffingdata,(err,data)=>{
-    console.log('inside',data)
-})
+let year = 2015
+
+// =============================================================================
+// ----------------------------[ IMPORT STAFFING DATA ]-------------------------
+
+var filetext = fs.readFileSync('./2015_data/2015.staffing.programs.csv','utf8')
+
+var records = parse(filetext, {auto_parse:true})
+
+var budgetstaffing = budgetroot.DataSeries.BudgetStaffing.Items
+
+for (var line of records) {
+    let code = line[0],
+        desc = line[1],
+        perm = line[2],
+        temp = line[3],
+        total = line[4]
+
+    if (perm === '') perm = null
+    if (temp === '') temp = null
+    if (total === '') total = null
+
+    let item = budgetstaffing[code]
+    if (!item) {
+        console.log('code not found: ', code)
+    } else {
+        let years = item.years || {}
+        years[year] =
+            {
+                Amount:total,
+                Components:{
+                    FULL:{Amount:perm},
+                    PART:{Amount:temp}
+                }
+            }
+        item.years = years
+        // console.log(item)
+    }
+}
+
+// console.log(budgetstaffing)
+
+// =============================================================================
+// ----------------------------[ IMPORT EXPENSE DATA ]-------------------------
+
+var filetext = fs.readFileSync('./2015_data/2015.expenses.expenditures.csv','utf8')
+
+var records = parse(filetext, {auto_parse:true})
+
+var budgetexpenses = budgetroot.DataSeries.BudgetExpenses.Items
+
+for (var line of records) {
+    let program = line[0],
+        expenditure = line[2],
+        amount = line[5]
+
+    if (amount === '') amount = null
+
+    let item = budgetexpenses[program]
+    if (!item) {
+        console.log('expense program code not found: ', program)
+    } else {
+        let years = item.years || {}
+        let yearobj = years[year] || {
+            Amount:0,
+            Components:{}
+        }
+        yearobj.Amount += amount
+        yearobj.Components[expenditure] = {Amount:amount}
+        years[year] = yearobj
+        item.years = years
+        // console.log(item)
+    }
+}
+
+// console.log(budgetstaffing)
+
+// =============================================================================
+// ----------------------------[ IMPORT EXPENSE DATA ]-------------------------
+
+var filetext = fs.readFileSync('./2015_data/2015.revenues.expenditures.csv','utf8')
+
+var records = parse(filetext, {auto_parse:true})
+
+var budgetrevenues = budgetroot.DataSeries.BudgetRevenues.Items
+
+for (var line of records) {
+    let program = line[0],
+        expenditure = line[2],
+        amount = line[5]
+
+    if (amount === '') amount = null
+
+    let item = budgetrevenues[program]
+    if (!item) {
+        console.log('revenue program code not found: ', program)
+    } else {
+        let years = item.years || {}
+        let yearobj = years[year] || {
+            Amount:0,
+            Components:{}
+        }
+        yearobj.Amount += amount
+        yearobj.Components[expenditure] = {Amount:amount}
+        years[year] = yearobj
+        item.years = years
+        // console.log(item)
+    }
+}
+
+// console.log(budgetstaffing)
+
+// ======================================================================
+// -------------------------[ SAVE OUTPUT JSON FILE ]--------------------
+
+jsonfile.spaces = 4
+
+jsonfile.writeFile('./2015budget.json', budgetroot)
