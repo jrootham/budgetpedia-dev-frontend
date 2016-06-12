2a13a1898fe366d076c25683577e1878c1eb23c4
Henrik Bechmann <henrik@therefore.ca>
2016-06-12 06:24:04 -0400

    embed GDrive project folder listing in About page

--- js/apps/controllers/about.js
+++ js/apps/controllers/about.js
@@ -1,10 +1,5 @@
 "use strict";
 const React = require('react');
-var { Component } = React;
-class About extends Component {
-    render() {
-        return React.createElement("div", null, "About Page");
-    }
-}
+const About = () => React.createElement("div", null, React.createElement("p", null, "This is a list of resources in the GDrive project repository"), React.createElement("iframe", {src: "https://drive.google.com/embeddedfolderview?id=0B208oCU9D8OuSms5bEZxcnRLZzA#list", style: { width: "100%", height: "600px", border: 0 }}));
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = About;
--- ts/apps/controllers/about.tsx
+++ ts/apps/controllers/about.tsx
@@ -1,12 +1,12 @@
 // about.tsx
-// required by bundler
 import * as React from 'react'
-var { Component } = React
 
-class About extends Component<any, any> {
-    render() {
-        return <div>About Page</div>
-    }
-}
+const About = () => 
+    <div>
+        <p>This is a list of resources in the GDrive project repository</p>
+        <iframe src="https://drive.google.com/embeddedfolderview?id=0B208oCU9D8OuSms5bEZxcnRLZzA#list" 
+            style={{width:"100%", height:"600px", border:0}}>
+        </iframe>
+    </div>
 
 export default About
