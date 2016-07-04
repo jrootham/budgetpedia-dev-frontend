53320adf9c9d1803c43aa56d8c9f80527d181002
Henrik Bechmann <henrik@therefore.ca>
2016-07-04 13:03:32 -0400

    remove maintiles commented out code from reducers and initialstate files

--- ts/core/reducers/reducers.tsx
+++ ts/core/reducers/reducers.tsx
@@ -37,65 +37,6 @@ let colors = (state: any = initialstate.colors) => {
     return state
 }
 
-// let mainpadding = (state: any = initialstate.mainpadding, action) => {
-//     return state
-// }
-
-/*let maintiles = (state: any = initialstate.maintiles, action) => {
-    return state
-}
-*/
-// ========================================================================
-// -----------------------[ SET TILE COLUMNS ]-----------------------------
-
-// let maincolsreducer = (state: any = initialstate.maincols, action) => {
-//     switch (action.type) {
-//         case Actions.SET_TILECOLS: {
-
-//             let mainElement = document.getElementById('main')
-
-//             let elementwidth: number = mainElement.getBoundingClientRect().width
-
-//             let columns: number;
-
-//             // breakpoints should be parameterized
-//             if (elementwidth > 960) {
-
-//                 columns = 4
-
-//             } else if (elementwidth > 680) {
-
-//                 columns = 3
-
-//             } else if (elementwidth > 400) {
-
-//                 columns = 2
-
-//             } else {
-
-//                 columns = 1
-
-//             }
-
-//             return columns
-
-//         }
-//         default:
-
-//             return state
-
-//     }
-// }
-
-// handleAction not usable with combineReducers
-// https://github.com/acdlite/redux-actions/issues/23
-// let maincols = handleAction(Actions.SET_TILECOLS, maincolsreducer)
-
-// compatible with flux standard action; quickly filter out non-matching calls
-// let maincols = handleActions({
-//     [Actions.SET_TILECOLS]: maincolsreducer,
-// }, initialstate.maincols )
-
 let workingmessagestate = (state:any = initialstate.workingmessagestate, action) => {
     switch (action.type) {
         case Actions.SHOW_WORKING_MESSAGE: {
