c1c17b18f0c244bc8229940a0093bbf449f93cba
Henrik Bechmann <henrik@therefore.ca>
2016-08-09 20:00:40 -0400

    format dialog help

--- ts/addins/explorer/content/helpcontent.tsx
+++ ts/addins/explorer/content/helpcontent.tsx
@@ -22,22 +22,25 @@ let content =
     <CardText expandable style={{paddingTop:"0px"}}>
     <p>There are several dataset options available for charts:</p>
     <ul>
-    <li><strong>Viewpoint:</strong> broad approaches to budget (or actual) data, based on similar 
-        classification schemes (taxonomies), and data sources.</li>
-    <li><strong>Version:</strong> alternate detailed datasets available for the selected Viewpoint</li>
-    <li><strong>Aspect:</strong> particular aspects of the budget, such as Revenue, Expenses or Staffing</li>
-    <li><strong>By Unit:</strong> the beginnings of diagnostic analytics, using simple math to view the 
-        data compared with relevant related metrics, such as per person or per household</li>
-    <li><strong>Inflation adjusted:</strong> the datasets are updated annually to reflect the Bank of 
-        Canada's Inflation Calculator. This makes it possible to get historical perspectives that are
-        meaningful in today's dollar terms.
-    </li>
+        <li><strong>Viewpoint:</strong> broad approaches to budget (or actual) data, based on similar 
+            classification schemes (taxonomies), and data sources.</li>
+        <li><strong>Version:</strong> alternate detailed datasets available for the selected Viewpoint</li>
+        <li><strong>Aspect:</strong> particular aspects of the budget, such as Revenue, Expenses or Staffing</li>
+        <li><strong>By Unit:</strong> the beginnings of diagnostic analytics, using simple math to view the 
+            data compared with relevant related metrics, such as per person or per household</li>
+        <li><strong>Inflation adjusted:</strong> the datasets are updated annually to reflect the Bank of 
+            Canada's Inflation Calculator. This makes it possible to get historical perspectives that are
+            meaningful in today's dollar terms.
+        </li>
     </ul>
     <p>Choices for the selections are interdependent. Toronto budgets have similar classifications for 
         Revenue and Expenses for example, and therefore these can be viewed together (as offered by the 
         <em>both</em> and <em>net</em> Aspects). Financial statements 
-        on the other hand do not have similar classifications for Revenue and Expenses.</p>
+        on the other hand do not have similar classifications for Revenue and Expenses.
+    </p>
+
     <hr />
+
     <p><strong><em>Viewpoint</em></strong> choices include:</p>
     <dl>
         <dt><strong>Budget (by function)</strong></dt>
@@ -62,107 +65,129 @@ let content =
             general picture of the ways that money is spent.
         </dd>
     </dl>
+
     <hr />
+
     <p>
         <strong><em>Version</em></strong> choices depend on the Viewpoint that is chosen.
     </p>
     <p><em>Budget viewpoint  Versions</em> (for both functional and structural Viewpoints) include:</p>
-    <ul>
-    <li><strong>Summary</strong></li>
-    <li><strong>Detail (FPARS)</strong></li>
-    <li><strong>Variance Reports</strong></li>
-    </ul>
+    <blockquote>
+        <ul>
+        <li>Summary</li>
+        <li>Detail (FPARS)</li>
+        <li>Variance Reports</li>
+        </ul>
+    </blockquote>
     <p><em>Financial statement viewpoint Versions</em> include:</p>
-    <ul>
-    <li>Audited Financial Statements</li>
-    <li>Financial Information Returns (FIRs for MMAH)</li>
-    </ul>
+    <blockquote>
+        <ul>
+        <li>Audited Financial Statements</li>
+        <li>Financial Information Returns (FIRs for MMAH)</li>
+        </ul>
+    </blockquote>
+
     <hr />
+
     <p>
         <strong><em>Aspect</em></strong> choices depend on the Viewpoint that is chosen.
     </p>
-    <p><em>Budget Viewpoint  Aspects</em> (for both functional and structural Viewpoints) include:</p>
-    <ul>
-    <li>Revenue</li>
-    <li>Expenses</li>
-    <li>Both</li>
-    <li>Net</li>
-    <li>Staffing</li>
-    </ul>
+        <p><em>Budget Viewpoint  Aspects</em> (for both functional and structural Viewpoints) include:</p>
+    <blockquote>
+        <ul>
+        <li>Revenue</li>
+        <li>Expenses</li>
+        <li>Both</li>
+        <li>Net</li>
+        <li>Staffing</li>
+        </ul>
     <p> All of these are based on City documents which have similar line items across all aspects. This 
         makes the figures from all sources comparable.
     </p>
+    </blockquote>
 
-    <p><em>Financial statement Viewpoint Aspects</em> include:</p>
-    <ul>
-    <li>Revenue</li>
-    <li>Expenses</li>
-    </ul>
+        <p><em>Financial statement Viewpoint Aspects</em> include:</p>
+    <blockquote>
+        <ul>
+        <li>Revenue</li>
+        <li>Expenses</li>
+        </ul>
+
     <p>The classification schemes for these are different for each, and therefore they cannot be 
         combined.
     </p>
-    <p><em>Expenses by Object Viewpoint Aspects</em> include:</p>
-    <ul>
-    <li>Expenses</li>
-    </ul>
+    </blockquote>
+        <p><em>Expenses by Object Viewpoint Aspects</em> include:</p>
+    <blockquote>
+        <ul>
+        <li>Expenses</li>
+        </ul>
     <p>These come from notes to the audited financial statements.
     </p>
+    </blockquote>
 
     <p><em>Variance report Version of budget Viewpoint Aspects</em> include:</p>
-    <ul>
-    <li>Revenue
+    <blockquote>
         <ul>
-        <li>Budget Revenue</li>
-        <li>Actual Revenue</li>
-        <li>Both Revenues</li>
-        <li>Revenue Variance</li>
+        <li>Revenue
+            <ul>
+            <li>Budget Revenue</li>
+            <li>Actual Revenue</li>
+            <li>Both Revenues</li>
+            <li>Revenue Variance</li>
+            </ul>
+        </li>
+        <li>Expenses
+            <ul>
+            <li>Budget Expenses</li>
+            <li>Actual Expenses</li>
+            <li>Both Expenses</li>
+            <li>Expenses Variance</li>
+            </ul>
+        </li>
+        <li>Both (Revenue & Expenses)
+            <ul>
+            <li>Budget</li>
+            <li>Actual</li>
+            <li>Both Budget & Actual</li>
+            <li>Variances</li>
+            </ul>
+        </li>
+        <li>Net (Revenue & Expenses)
+            <ul>
+            <li>Budget</li>
+            <li>Actual</li>
+            <li>Both Budget & Actual</li>
+            <li>Variances</li>
+            </ul>
+        </li>
+        <li>Staffing
+            <ul>
+            <li>Budget Staffing</li>
+            <li>Actual Staffing</li>
+            <li>Both Staffing</li>
+            <li>Staffing Variance</li>
+            </ul>
+        </li>
         </ul>
-    </li>
-    <li>Expenses
-        <ul>
-        <li>Budget Expenses</li>
-        <li>Actual Expenses</li>
-        <li>Both Expenses</li>
-        <li>Expenses Variance</li>
-        </ul>
-    </li>
-    <li>Both (Revenue & Expenses)
-        <ul>
-        <li>Budget</li>
-        <li>Actual</li>
-        <li>Both Budget & Actual</li>
-        <li>Variances</li>
-        </ul>
-    </li>
-    <li>Net (Revenue & Expenses)
-        <ul>
-        <li>Budget</li>
-        <li>Actual</li>
-        <li>Both Budget & Actual</li>
-        <li>Variances</li>
-        </ul>
-    </li>
-    <li>Staffing
-        <ul>
-        <li>Budget Staffing</li>
-        <li>Actual Staffing</li>
-        <li>Both Staffing</li>
-        <li>Staffing Variance</li>
-        </ul>
-    </li>
-    </ul>
+    </blockquote>
 
     <hr />
+
     <p>
         <strong><em>By Unit</em></strong> choices are common for all other choices, and include:
     </p>
+    <blockquote>
     <ul>
-    <li>Per staffing position</li>
-    <li>Population: per person</li>
-    <li>Population: per 100,000 people</li>
-    <li>Per household</li>
+        <li>Per staffing position</li>
+        <li>Population: per person</li>
+        <li>Population: per 100,000 people</li>
+        <li>Per household</li>
     </ul>
+    </blockquote>
+
     <hr />
+
     <p>
         <strong><em>Inflation adjusted</em></strong> is on by default, but can be turned off. This uses
         the Bank of Canada's Inflation Calculator to adjust historical figures in terms of recent 
