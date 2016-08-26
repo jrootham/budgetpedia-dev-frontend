ac88f92a40c69a5f40848202ede055396089bfa7
Henrik Bechmann <henrik@therefore.ca>
2016-08-26 13:25:31 -0400

    structure basic run cli app

--- db/code/run.js
+++ db/code/run.js
@@ -1,24 +1,75 @@
 // run.js
 
+/*
+    run help
+    run create-dataset <repository>
+    run add-to-dataset <repository> [filename]
+*/
+
 var fs = require('fs');
 
-(function() {
+let context = {};
 
-let parms = process.argv.splice(2)
+(function(context) {
 
-let repository = parms[0]
-
-let dbroot = './repositories/' // relative to process.cwd()
-
-let dirs = fs.readdirSync(dbroot)
-
-if (!repository || (dirs.indexOf(repository) == -1)) {
-
-    console.log('no repository specified, or repository not found. Use node code/run <repository>')
+try {
+    if (!setup(context)) return
+} catch (e) {
+    // console.log(e)
     return
-
 }
 
 console.log('Done.')
 
-})()
+})(context)
+
+function setup(context) {
+    context.parms = process.argv.splice(2)
+
+    let command = context.command = context.parms[0]
+    context.repository = context.parms[1]
+    context.filename = context.parms[2]
+
+    context.dbroot = './repositories/' // relative to process.cwd()
+
+    context.dirs = fs.readdirSync(context.dbroot)
+
+    switch (command) {
+        case 'help': {
+            showHelp()
+            return false
+        } 
+        break
+        case 'create-dataset':
+        case 'add-to-dataset': {
+            if ((!context.repository) || (context.dirs.indexOf(context.repository) == -1)) {
+
+                console.error('no repository specified, or repository not found. Use node code/run <repository>')
+                throw Error()
+
+            }
+        }
+        break
+        default: {
+            console.error('command not recognized. try node code/run help')
+        }
+    }
+
+    return true
+}
+
+function showHelp() {
+    console.log(
+`
+syntax 
+    npm code/run <command> <parameters>
+
+commands
+    help
+
+    create-dataset <repository>
+
+    add-to-dateset <repository> [<filename>]
+`
+    )
+}
